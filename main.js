be36e167eb68719fc615cfe339b092e786652d4f
Vlad <2000californiadream2000@gmail.com>
2021-07-22 12:50:40 +0300

    decomposition done

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const StellarHDWallet = require('stellar-hd-wallet');
+const StellarSDK = require('stellar-sdk');
 const forge = require("node-forge");
 
 const password = 'password';
@@ -16,6 +17,11 @@ console.debug("WALLET: ", wallet);
 console.debug("PK from raw: ", wallet.getPublicKey(0));
 console.debug("SK from raw: ", wallet.getSecret(0));
 
+// let genKeypair = StellarSDK.Keypair.fromSecret(wallet.getSecret(0));
+
+// console.debug("PK from SK: ", genKeypair.publicKey() );
+// console.debug("SK from SK: ", genKeypair.secret() );
+
 // const accFromSeed = StellarHDWallet.fromSeed(wallet.seedHex);
 
 // console.debug("PK from seed: ", accFromSeed.getPublicKey(0));
@@ -27,22 +33,41 @@ function formatPass(pass) {
     return key;
 }
 
-const sk = forge.util.createBuffer(wallet.getPublicKey(0));
+function encrypt(payload, key) {
+    const sk = forge.util.createBuffer(payload);
 
-var cipher = forge.cipher.createCipher('AES-CBC', formatPass(password));
-cipher.start({iv: sixteenBytes});
-cipher.update(sk);
-cipher.finish();
-var encrypted = cipher.output;
-console.log("ENCRYPTED: ", encrypted);
+    let cipher = forge.cipher.createCipher('AES-CBC', key);
+    cipher.start({iv: sixteenBytes});
+    cipher.update(sk);
+    cipher.finish();
+    let encrypted = cipher.output;
+    console.debug("ENCRYPTED: ", encrypted);
 
-let ciphertext = encrypted.getBytes();
+    let ciphertextHEX = encrypted.toHex(); //encrypted.getBytes();
+    console.debug("ENCRYPTED HEX", ciphertextHEX);
 
-var decipher = forge.cipher.createDecipher('AES-CBC', formatPass(password));
-decipher.start({iv: sixteenBytes});
-decipher.update(forge.util.createBuffer(ciphertext));
-var result = decipher.finish();
-
-if(result) {
-    console.log("HEX dectypted: ", decipher.output.data);   
+    return ciphertextHEX;
 }
+
+function decrypt(cipherHEX, key) {
+    let ciphertext = forge.util.hexToBytes(cipherHEX);
+    //console.debug("ENCRYPTED bytes", ciphertext);
+    
+    let decipher = forge.cipher.createDecipher('AES-CBC', key);
+    decipher.start({iv: sixteenBytes});
+    decipher.update(forge.util.createBuffer(ciphertext));
+    let result = decipher.finish();
+    if(result) {
+        console.debug("HEX dectypted: ", decipher.output.data);
+        return decipher.output.data;  
+    } else {
+        console.error("Decryption failed");
+    }
+}
+
+
+let encrypted = encrypt(wallet.getPublicKey(0), formatPass(password));
+let decrypted = decrypt(encrypted, formatPass(password));
+
+console.log("ENC: ", encrypted);
+console.log("DEC: ", decrypted);
